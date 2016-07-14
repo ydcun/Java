82789866965ba035c5991c2fe6554271a18c0783
ydcun@hotmail.com <ydcun@hotmail.com>
2016-07-14 20:01:12 +0800

    add leetcode package

--- java/src/main/java/com/ydcun/java/App.java
+++ java/src/main/java/com/ydcun/java/App.java
@@ -4,10 +4,39 @@ package com.ydcun.java;
  * Hello world!
  *
  */
-public class App 
+class Base{
+	int i=2;
+	public Base(){
+		this.display();
+	}
+	public void display() {
+		System.out.println(this.i);
+	}
+	
+}
+class Derived extends Base{
+	int i=22;
+	public Derived(){
+	}
+	public void display(){
+		System.out.println(i);
+	}
+}
+public class App extends Derived
 {
+	int i=222;
+	public void accesMid(){
+		System.out.println(super.i);
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	new Base();
+    	App app = new App();
+    	Derived derived= app;
+    	Base base = derived;
+    	System.out.println(app.i);
+    	System.out.println(derived.i);
+    	System.out.println(base.i);
+//    	derived.accesMid();
     }
 }
---
+++ java/src/main/java/com/ydcun/java/leetcode/PowerOfFour.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ydcun.java.leetcode;
+
+/**
+ * @author ydcun-psjs
+ *
+ */
+public class PowerOfFour {
+    public boolean isPowerOfFour(int num) {
+        if(num <= 0)
+            return false;
+        if((num&(num-1))>0)
+            return false;
+        return (num&0x55555555)>0; 
+    }
+    public static void main(String[] args) {
+    	PowerOfFour pof = new PowerOfFour();
+		for(int i=0;i<100;i++){
+			if(pof.isPowerOfFour(i)){
+				System.out.println("ok"+i);
+			}else{
+				System.out.println("false"+i);
+			}
+		}
+	}
+}

3acdd900979cbd1ae6a1d6c68834fc080588c439
ydcun@hotmail.com <ydcun@hotmail.com>
2016-07-18 11:20:28 +0800

    update cache format

--- java/src/main/java/com/ydcun/java/thread/CacheDemo.java
+++ java/src/main/java/com/ydcun/java/thread/CacheDemo.java
@@ -10,31 +10,34 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  *
  */
 public class CacheDemo {
- private Map<String,Object> cache = new HashMap<String,Object>();
- public static void main(String[] args) {
-  
- }
- private ReadWriteLock rwl = new ReentrantReadWriteLock();
- public Object get(String name){
-  rwl.readLock().lock();
-  Object value =null;
-  try{
-   value = cache.get(name);
-   if(value==null){
-    rwl.readLock().unlock();
-    rwl.writeLock().lock();
-    try{
-     if(value==null){
-      value="aaa";//去数据库里取值
-     }
-    }finally{
-     rwl.writeLock().unlock();
-    }
-    rwl.readLock().lock();
-   }
-  }finally{
-   rwl.readLock().unlock();
-  }
-  return value;
- }
+	private Map<String, Object> cache = new HashMap<String, Object>();
+
+	public static void main(String[] args) {
+
+	}
+
+	private ReadWriteLock rwl = new ReentrantReadWriteLock();
+
+	public Object get(String name) {
+		rwl.readLock().lock();
+		Object value = null;
+		try {
+			value = cache.get(name);
+			if (value == null) {
+				rwl.readLock().unlock();
+				rwl.writeLock().lock();
+				try {
+					if (value == null) {
+						value = "aaa";// 去数据库里取值
+					}
+				} finally {
+					rwl.writeLock().unlock();
+				}
+				rwl.readLock().lock();
+			}
+		} finally {
+			rwl.readLock().unlock();
+		}
+		return value;
+	}
 }

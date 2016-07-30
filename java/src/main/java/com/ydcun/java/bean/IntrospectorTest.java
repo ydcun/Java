6f77a31bce5c99d2c1623efcea385f8af63a166c
ydcun@hotmail.com <ydcun@hotmail.com>
2016-07-30 14:36:44 +0800

    add BeanUtils 注册非基本类型自动转换器

--- java/src/main/java/com/ydcun/java/bean/IntrospectorTest.java
+++ java/src/main/java/com/ydcun/java/bean/IntrospectorTest.java
@@ -7,8 +7,14 @@ import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.ConversionException;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.Converter;
 import org.apache.commons.beanutils.PropertyUtils;
 
 import com.ydcun.java.reflection.ReflectPoint;
@@ -33,9 +39,38 @@ public class IntrospectorTest {
 
 		//用apache提供的BeanUtil工具类（commons-beanutils  commons-logging的支持）
 		System.out.println(BeanUtils.getProperty(pt1, "x").getClass().getName());
-		BeanUtils.setProperty(pt1, "x","9");
+		BeanUtils.setProperty(pt1, "x","9");//bean中的八种数据类型方法传入的是字符串是可以自动给转换的
 		System.out.println(pt1.getX());
-
+		
+		
+//		BeanUtils.setProperty(pt1, "birthday","2016-07-30");//不能将非基本数据类型自动转换
+		ConvertUtils.register(new Converter() {
+			SimpleDateFormat sb = new SimpleDateFormat("yyyy-MM-dd");
+			@Override
+			public Object convert(Class type, Object value) {
+				if(value==null){
+					return null;
+				}
+				if(!(value instanceof String)){
+					throw new ConversionException("not convert, only String type");
+				}
+				String str = (String)value;
+				if(str.trim().equals("")){
+					return null;
+				}
+				Date d = null;
+				try {
+					d = sb.parse((String)value);
+				} catch (ParseException e) {
+					e.printStackTrace();
+					throw new RuntimeException(e);//异常链不能断
+				}
+				return d;
+			}
+		}, Date.class);
+		BeanUtils.setProperty(pt1, "birthday","2016-07-30");
+		System.out.println(pt1.getBirthday().toString());
+		
 		//支持级联操作  类型工具会帮助你转换
 		BeanUtils.setProperty(pt1, "birthday.time", "111");
 		System.out.println(BeanUtils.getProperty(pt1, "birthday.time"));

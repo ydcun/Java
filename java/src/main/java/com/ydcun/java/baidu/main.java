0091764a70240a75b795871f35c328db99c7870e
ydcun@hotmail.com <ydcun@hotmail.com>
2016-09-20 17:50:45 +0800

    update baidu

--- java/src/main/java/com/ydcun/java/baidu/main.java
+++ java/src/main/java/com/ydcun/java/baidu/main.java
@@ -3,47 +3,48 @@
  */
 package com.ydcun.java.baidu;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @author ydcun-psjs
  *
  */
 public class main {
-	public static void main(String[] args) {
-
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader strin=new BufferedReader(new InputStreamReader(System.in));
+		String str = strin.readLine();
+		Map<Character,Integer> base = new HashMap<Character,Integer>();
+		Character temp = str.charAt(0);
+		int length=0;
+		for(int i=0;i<str.length();i++){
+			Character tempChar = str.charAt(i);
+			if(temp.equals(tempChar)){
+				length++;
+			}else{
+				Integer sum = base.get(temp);
+				if(sum==null){
+					sum=0;
+				}
+				if(length>sum){
+					sum = length;
+				}
+				base.put(temp, sum);
+				length=0;
+				temp = tempChar;
+				i--;
+			}
+		}
+		int s = 0;
+		for(Character c:base.keySet()){
+			s+=base.get(c);
+		}
+		System.out.println(s);
 		
 		
 	}
 }
-
-
-
-int longestValidParentheses(string s) {
-		if (s.size() <= 0)return 0;
-		stack<char> paren;
-		stack<int> pos;
-		pos.push(-1);
-		int max = 0;
-		for (size_t i = 0; i < s.size(); i++)
-		{
-			if (s[i] == '(')
-			{
-				paren.push(s[i]);
-				pos.push(i);
-			}
-			else if (!paren.empty()&&paren.top()=='(')
-			{
-				paren.pop();
-				pos.pop();
-				max = max < i - pos.top() ? i - pos.top() : max;
-			}
-			else
-			{
-				paren.push(s[i]);
-				pos.push(i);
-			}
-		}
-		if(max==s.length())return max/2;
-		return -1;
-	}
-

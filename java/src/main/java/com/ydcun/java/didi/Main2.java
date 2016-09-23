313a7120735c8953e51a04b321a75e097fa2e334
ydcun@hotmail.com <ydcun@hotmail.com>
2016-09-23 16:25:03 +0800

    sum 方案2

--- java/src/main/java/com/ydcun/java/didi/Main2.java
+++ java/src/main/java/com/ydcun/java/didi/Main2.java
@@ -2,19 +2,20 @@ package com.ydcun.java.didi;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
  *求任意一个数组中所有和为给定数的组合个数
  */
-public class Main {
+public class Main2 {
 
     private static int count = 0;
     private static int []array;
     private static int sum;
     
     static int getCount() {
-        Main.quickSort(array, 0, array.length-1);
-        Main.sum(array.length-1);
+        Arrays.sort(array);
+        Main2.sum(array.length-1);
         return count;
     }
     
@@ -44,40 +45,6 @@ public class Main {
         
         return a;
     }
-    
-    private static void quickSort(int []array, int left, int right) {
-        if(left >= right) {
-            return;
-        }
-        int q = pagenation(array, left, right);
-        quickSort(array, left, q-1);
-        quickSort(array, q+1, right);
-    }
-    
-    private static int pagenation(int []array, int left, int right) {
-        int i=left, j=right+1;
-        int p = (int)Math.random()*(right-left+1)+left;
-        swap(array,left, p);
-        int x = array[left];
-        
-        while(true) {
-            while(i<right && array[++i] < x);
-            while(j>left && array[--j] > x);
-            if(i > j) {
-                break;
-            }
-            swap(array,i,j);
-        }
-        return j;
-    }
-    
-    private static void swap(int [] array, int i, int j) {
-    	if(i==j) return;
-        array[i] +=array[j];
-        array[j] = array[i]-array[j];
-        array[i] = array[i] - array[j];
-    }
-    
     public static void main(String[] args) throws Exception {
     	BufferedReader strin=new BufferedReader(new InputStreamReader(System.in));
 		String str = strin.readLine();
@@ -88,7 +55,7 @@ public class Main {
 		for(int i=0;i<Integer.parseInt(arr[0]);i++){
 			array[i] = Integer.parseInt(arrStr[i]);
 		}
-        System.out.println(Main.getCount());
+        System.out.println(Main2.getCount());
     }
     
 }
